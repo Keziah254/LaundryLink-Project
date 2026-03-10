e01ce1a1959b7da060c38694b8c8dca7557844bc
Keziah254 <keziahnzisa@gmail.com>
2026-03-11 00:50:47 +0300

    Fix: Added content to PWA files

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,27 +1,33 @@
-const CACHE_NAME = "laundrylink-v1";
-
+const CACHE_NAME = 'laundrylink-cache-v1';
 const urlsToCache = [
-  "/",
-  "/login",
-  "/register",
-  "/dashboard",
-  "/static/style.css"
+  '/',
+  '/static/css/style.css',
+  '/static/js/main.js', // If you have a main JS file
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png'
 ];
 
-// Install
-self.addEventListener("install", event => {
+// Install event: Cache files
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Fetch
-self.addEventListener("fetch", event => {
+// Fetch event: Serve from cache, fall back to network
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
